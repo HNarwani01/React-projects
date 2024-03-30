b04afe531d3949334f6a0fd92c48c1c3b69655d6
HNarwani01 <himanshurawat0075407@gmail.com>
2024-03-30 15:44:02 +0530

    social media 0.1

--- To-Do-List-Reduce/src/App.jsx
+++ To-Do-List-Reduce/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Thead from "./components/Thead.jsx";
@@ -6,38 +6,64 @@ import Tdata from "./components/Tdata.jsx";
 import WelcomeMessage from "./components/Welcome.jsx";
 import { WorkList } from "./store/WorkList.jsx";
 
+const todoitemReducer = (state, action) => {
+  let changes = state;
+  if (action.type === 'NEW_ITEM') {
+    changes = [...state, {work:action.payload.workTodo,Time:action.payload.time }]
+  } else if (action.type === 'REMOVE_ITEM') {
+    changes = state.filter((item, index) => index !== action.payload);
+  }
+  return changes;
+}
+
+
 function App() {
-  const toDoItemsFromContext =useContext(WorkList);
-  
-  const [work, setWork] = useState(toDoItemsFromContext);
-  
+  const [work, dispatchitem] = useReducer(todoitemReducer, [])
+  // const toDoItemsFromContext = useContext(WorkList);
+
+  // const [work, setWork] = useState(toDoItemsFromContext);
+console.log(work);
   const addToWork = (workTodo, time) => {
-    if (workTodo !== '' && time !== '') {
-      setWork((currentValue)=>[...currentValue, {work: workTodo,Time: time}]);
-    }
+    const newitemadd = {
+      type: 'NEW_ITEM',
+      payload: { workTodo, time },
+      
+    };
+    dispatchitem(newitemadd)
+
   };
   
   const removefromwork = (i) => {
-    let newWork = [...work]; // Create a copy of the original array
-    newWork.splice(i, 1); // Remove the item at index i from the copied array
-  
-    setWork(newWork); // Update the state with the modified array
+    console.log(i);
+    const itemremove = {
+      type: 'REMOVE_ITEM',
+      payload: i
+    }
+    dispatchitem(itemremove)
+    
   };
-  const defaultTodo = [{
-    work: 'Cleaning House',
-    Time: '20/03/2024'
-  }]
+  // if (workTodo !== '' && time !== '') {
+  //   setWork((currentValue)=>[...currentValue, {work: workTodo,Time: time}]);
+  // }
+  // let newWork = [...work]; 
+  // newWork.splice(i, 1); 
+  
+  // setWork(newWork); 
+  // const defaultTodo = [{
+  //   work: 'Cleaning House',
+  //   Time: '20/03/2024'
+  // }]
   return (
-    <WorkList.Provider value={{work,removefromwork,addToWork}}>
-    <center className="center-tag">
-      <table className="table table-success table-striped">
-      <Thead  />
-        <tbody>
-          <Tdata/>
-        </tbody>
-      </table>
-      <WelcomeMessage/>
-    </center>
+    <WorkList.Provider value={{ work, removefromwork, addToWork }}>
+      <center className="center-tag">
+        <table className="table table-success table-striped">
+          <Thead />
+          <tbody>
+            <Tdata />
+          </tbody>
+        </table>
+        <WelcomeMessage />
+      </center>
     </WorkList.Provider>
   );
 }
